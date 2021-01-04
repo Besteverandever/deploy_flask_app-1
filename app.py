84bf7ed93174df71fdf2ce6773f537569186530e
Universe2020 <sadafwaqas@gmail.com>
2021-01-04 10:45:09 +0000

    commit

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def predict():
 
 if __name__ == "__main__":
     port=int(os.environ.get('PORT',5000))
-    app.run(port=port,debug=True,use_reloader=False)
+    app.run(port=port,debug=True,use_reloader=False)    
